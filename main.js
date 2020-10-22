1984a1206ec99f089728b731ad99676a21617dc1
Adri Zhao <zhaocx@bc.edu>
2020-10-21 21:06:18 -0400

    finished

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-// sort direction for bars in bar chart
-// 0 for ascending, 1 for descending
-let sort_type = 0
-// data category to group by 
-// either 'stores' or 'revenue'
+let sortType = 0
 let category = 'stores'
 
 const m = 80
@@ -23,15 +19,16 @@ const xScale = d3.scaleBand()
     .paddingInner(0.1)
 
 const yScale = d3.scaleLinear()
-    .rangeRound([0, height])
+    .range([height, 0])
 
-svg.append('g').attr('class', 'axis y-axis')
 svg.append('g').attr('class', 'axis x-axis')
+svg.append('g').attr('class', 'axis y-axis')
+
 
 yAxisLabel = svg.append('text')
     .attr('x', -70)
     .attr('y', -10)
-    .attr('fill', '#876957')
+    .attr('fill', '#557387')
     .text('Number of Stores')
 
 
@@ -41,21 +38,21 @@ function update(data, category) {
     // update data
     category = document.querySelector('#category').value
     data = data.sort(function (a, b) {
-        if (sort_type == 0) return a[category] - b[category]
-        if (sort_type == 1) return b[category] - a[category]
+        if (sortType == 0) return a[category] - b[category]
+        if (sortType == 1) return b[category] - a[category]
     })
 
     // update domains
     xScale.domain(data.map(d => d.company))
-    yScale.domain(d3.extent(data, d => d[category]))
+    yScale.domain([0, d3.max(data, d => d[category])])
 
-    const xAxis = d3.axisBottom().scale(xScale).ticks(5, 's')
-    const yAxis = d3.axisLeft().scale(yScale).ticks(5, 's')
+    const xAxis = d3.axisBottom().scale(xScale).ticks(8, 's')
+    const yAxis = d3.axisLeft().scale(yScale).ticks(8, 's')
 
     // update axes 
     yAxisLabel.text(function () {
         if (category == 'stores') return 'Number of Stores'
-        if (category == 'revenue') return 'Revenue in Billions ($)'
+        if (category == 'revenue') return 'Billion USD'
     })
 
     svg.select('.x-axis')
@@ -73,8 +70,8 @@ function update(data, category) {
         .transition()
         .duration(1000)
         .attr('x', d => xScale(d.company))
-        .attr('y', d => height - yScale(d[category]))
-        .attr('height', d => yScale(d[category]))
+        .attr('y', d => yScale(d[category]))
+        .attr('height', d => height - yScale(d[category]))
         .attr('width', 50)
 }
 
@@ -83,8 +80,8 @@ d3.csv('coffee-house-chains.csv', d3.autoType).then(data => {
     console.log('data', data)
 
     data = data.sort(function (a, b) {
-        if (sort_type == 0) return a[category] - b[category]
-        if (sort_type == 1) return b[category] - a[category]
+        if (sortType == 0) return a[category] - b[category]
+        if (sortType == 1) return b[category] - a[category]
     })
 
     xScale.domain(data.map(d => d.company))
@@ -102,7 +99,7 @@ d3.csv('coffee-house-chains.csv', d3.autoType).then(data => {
         .enter()
         .append('rect')
         .attr('class', 'rect')
-        .attr('fill', '#876957')
+        .attr('fill', '#add8e6')
         .attr('x', d => xScale(d.company))
         .attr('y', d => height - yScale(d[category]))
         .attr('height', d => yScale(d[category]))
@@ -115,7 +112,7 @@ d3.csv('coffee-house-chains.csv', d3.autoType).then(data => {
         update(data, category)
     })
     document.querySelector('#sort').addEventListener('click', () => {
-        sort_type == 0 ? sort_type = 1 : sort_type = 0
+        sortType == 0 ? sortType = 1 : sortType = 0
         update(data, category)
     })
 
